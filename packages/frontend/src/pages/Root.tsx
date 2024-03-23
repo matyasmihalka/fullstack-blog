8a04494071df1c985d1a4eb9eb6583ac441fa325
Matyas Mihalka <matyas.mihalka@applifting.cz>
2024-03-23 18:41:34 +0100

    fix: css nesting config and loading screen styling

--- packages/frontend/src/pages/Root.tsx
+++ packages/frontend/src/pages/Root.tsx
@@ -5,16 +5,18 @@ import { MainNav } from "../features/ui/composits/MainNav";
 const Root = () => {
   const { isAuthenticating } = useAuthCtx();
 
-  if (isAuthenticating) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="bg-zinc-50 dark:bg-zinc-950 flex flex-col h-screen">
-      <MainNav />
-      <div className="px-8 flex-grow overflow-auto">
-        <Outlet />
-      </div>
+      {isAuthenticating ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <MainNav />
+          <div className="px-8 flex-grow overflow-auto">
+            <Outlet />
+          </div>
+        </>
+      )}
     </div>
   );
 };
